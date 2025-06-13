37915259e317268d0a05ba23f576dde68a62d1ee
rbto-flores <roberto.floresrosas@pm.me>
2025-06-13 17:35:49 -0400

    commented

--- src/utils/contractListener.ts
+++ src/utils/contractListener.ts
@@ -7,13 +7,13 @@ const contractABI = [
     "event EventClosed(string indexed eventCode)" // <- Evento real
 ];
 console.log(process.env.WEBSOCKET_URL);
-const provider = new ethers.WebSocketProvider("wss://api.avax-test.network/ext/bc/C/ws");
+// const provider = new ethers.WebSocketProvider("wss://api.avax-test.network/ext/bc/C/ws");
 
-const contract = new ethers.Contract(
-    "0x91c21B7132b8305E352104ba1e18d302FbAf6162",
-    contractABI,
-    provider
-);
+// const contract = new ethers.Contract(
+//     "0x91c21B7132b8305E352104ba1e18d302FbAf6162",
+//     contractABI,
+//     provider
+// );
 
 export const startListening = () => {
     console.log("🟢 Escuchando eventos del contrato...");
