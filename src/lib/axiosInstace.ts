46d9878098b2f1bbde54de2fdece3f9751ae6397
Guido Salazar Vargas <guido.salazar.vargas7@gmail.com>
2025-06-14 17:56:45 -0400

    updated enviroments

--- src/lib/axiosInstace.ts
+++ src/lib/axiosInstace.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
 export const apiClient = axios.create({
     baseURL: 'https://twttrapi.p.rapidapi.com',
